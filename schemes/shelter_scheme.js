c85aeb6453efd1a334cbf28f151a0bb8fa7bc8a8
omrinelken <omri.nelken@gmail.com>
2024-08-11 18:46:11 +0300

    updated backend files

--- schemes/shelter_scheme.js
+++ schemes/shelter_scheme.js
@@ -1,50 +1,48 @@
-  const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-  //const todoSchema = new mongoose.Schema({
-    //  title: { type: String, required: true },
-      //isComplete: { type: Boolean, default: false },
-  //});
+//const todoSchema = new mongoose.Schema({
+//  title: { type: String, required: true },
+//isComplete: { type: Boolean, default: false },
+//});
 
-  const shelterSchema = new mongoose.Schema({
-      //title: { type: String, required: true },
-      //description: { type: String, required: true },
-      //body: { type: String, required: true },
-      //todoList: [todoSchema],
-      //isPinned: { type: Boolean, default: false },
-      //user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+const shelterSchema = new mongoose.Schema({
+  //title: { type: String, required: true },
+  //description: { type: String, required: true },
+  //body: { type: String, required: true },
+  //todoList: [todoSchema],
+  //isPinned: { type: Boolean, default: false },
+  //user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 
-          address: {
-              type: String,
-              required: true
-          },
-          capacity: {
-              type: Number,
-              required: true
-          },
-          coordinates: {
-              latitude: {
-                  type: Number,
-                  required: true
-              },
-              longitude: {
-                  type: Number,
-                  required: true
-              }
-          },
-          notes: {
-              type: String,
-              required: true
-          },
-          accessibility: {
-              type: Boolean,
-              required: true
-          },
+  address: {
+    type: String,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  coordinates: {
+    latitude: {
+      type: Number,
+      required: true,
+    },
+    longitude: {
+      type: Number,
+      required: true,
+    },
+  },
+  notes: {
+    type: String,
+    required: true,
+  },
+  accessibility: {
+    type: Boolean,
+    required: true,
+  },
 
+  isPrivate: { type: Boolean, required: true },
+  contact_name_and_phone_number: { type: String, required: true },
+});
 
-    //   isPrivate: {type:Boolean, required:true},
-    //   contact: {type:String, required:true},
-
-  });
-
-  const Shelter = mongoose.model("Shelter", shelterSchema);
-  module.exports = Shelter;
+const Shelter = mongoose.model("Shelter", shelterSchema);
+module.exports = Shelter;
