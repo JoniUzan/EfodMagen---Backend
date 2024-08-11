6a89a3fd72aa5a3c573e49b1ad6c1ac851f251dd
omrinelken <omri.nelken@gmail.com>
2024-08-11 14:33:27 +0300

    back end

---
+++ routers/shelters_router.js
@@ -0,0 +1,37 @@
+// const express = require("express");
+// const router = express.Router();
+// const {
+//     getUserTasks,
+//     deleteTaskById,
+//     updateTask,
+//     createTask,
+//     getTaskById,
+// } = require("../controllers/tasks_controller");
+// const { verifyToken } = require("../middleware/authentication_middleware");
+
+// router.get("/", verifyToken, getUserTasks);
+// router.get("/details/:id", verifyToken, getTaskById);
+// router.post("/create", verifyToken, createTask);
+// router.delete("/:id", verifyToken, deleteTaskById);
+// router.put("/:id", verifyToken, updateTask);
+
+// module.exports = router;
+
+const express = require("express");
+const router = express.Router();
+const {
+  findShelter,
+  //getShelterById
+  deleteShelterkById,
+  updateShelter,
+  createShelter,
+} = require("../controllers/shelters_controller");
+const { verifyToken } = require("../middleware/authentication_middleware");
+
+router.get("/", findShelter);
+//router.get("/details/:id", verifyToken, getShelterById);
+router.post("/", createShelter);
+router.delete("/:id", deleteShelterkById);
+router.put("/:id", updateShelter);
+
+module.exports = router;
---
+++ schemes/user_scheme.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    //username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    saved_shelters: [],
+    israeli_ID: {type:Number, required: true, unique: true},
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    address: { type: String, required: true},
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
